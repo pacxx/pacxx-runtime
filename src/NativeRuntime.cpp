5191355fb37a7f8270c2b3c3b2a9d9ed6c514f48
Lars Klein <l_klei14@wwu.de>
2016-10-06 09:46:31 +0200

    removed virtual (stupid)

--- src/NativeRuntime.cpp
+++ src/NativeRuntime.cpp
@@ -49,17 +49,17 @@ namespace pacxx
 
     void NativeRuntime::deleteRawMemory(RawDeviceBuffer* ptr) { throw common::generic_exception("not implemented"); }
 
-    virtual void initializeMSP(std::unique_ptr <llvm::Module> M) { throw common::generic_exception("not implemented"); }
+    void initializeMSP(std::unique_ptr <llvm::Module> M) { throw common::generic_exception("not implemented"); }
 
-      virtual void evaluateStagedFunctions(Kernel& K) { throw common::generic_exception("not implemented"); }
+    void evaluateStagedFunctions(Kernel& K) { throw common::generic_exception("not implemented"); }
 
-      virtual void requestIRTransformation(Kernel& K) { throw common::generic_exception("not implemented"); };
+    void requestIRTransformation(Kernel& K) { throw common::generic_exception("not implemented"); };
 
-      virtual const llvm::Module& getModule() { throw common::generic_exception("not implemented"); }
+    const llvm::Module& getModule() { throw common::generic_exception("not implemented"); }
 
-      virtual void synchronize() { throw common::generic_exception("not implemented"); };
+    void synchronize() { throw common::generic_exception("not implemented"); };
 
-      virtual llvm::legacy::PassManager& getPassManager() { throw common::generic_exception("not implemented"); };
+    llvm::legacy::PassManager& getPassManager() { throw common::generic_exception("not implemented"); };
 
   }
 }
