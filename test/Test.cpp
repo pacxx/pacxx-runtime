19dcfca0ab06f175ae09803d31bfb3987966fb2e
Lars Klein <l_klei14@wwu.de>
2016-10-12 08:43:22 +0200

    modified test

--- test/Test.cpp
+++ test/Test.cpp
@@ -14,26 +14,24 @@ using namespace std;
 int main(int argc, char **argv) {
   std::vector<int> a(OPT_N), b(OPT_N), c(OPT_N);
 
-  auto test = [](const vector<int> &a) {
-    auto idx = Thread::get().global.x;
-    if (idx < a.size())
+  auto test = [](int* a) {
       a[idx] = 20;
   };
 
   Executor& exec = get_executor<CUDARuntime>();
-  MemoryManager mm = exec.mm();
 
-  RawDeviceBuffer dev_a = mm.translateVector(a);
+  DeviceBuffer<int>& dev_a = exec.allocate(1);
 
-
-  auto vaddKernel =
+  Kernel vaddKernel =
       kernel<CUDARuntime>(test, {{(OPT_N + THREAD_N - 1) / THREAD_N}, {THREAD_N}});
 
-  vaddKernel(a);
+  vaddKernel(dev_a.get());
+
+
+  dev_a.download(&a.begin(), sizeof(int));
 
   exec.synchronize();
 
-  dev_a.download(a, OPT_N * sizeof(int));
 
   std::cout << a[0] << std::endl;
   return 0;
