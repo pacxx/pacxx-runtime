c2eb11d5df7a84a2f5a35bcf3c6b4c95f8bfd0ca
Lars <l_klei14@wwu.de>
2016-10-12 09:25:00 +0200

    fixed Test

--- test/Test.cpp
+++ test/Test.cpp
@@ -12,26 +12,27 @@ using namespace std;
 #define DIV_UP(a, b) (((a) + (b)-1) / (b))
 
 int main(int argc, char **argv) {
+
   std::vector<int> a(OPT_N), b(OPT_N), c(OPT_N);
 
+  size_t count = OPT_N;
+
   auto test = [](int* a) {
-      a[idx] = 20;
+      a[0] = 20;
   };
 
-  Executor& exec = get_executor<CUDARuntime>();
+  auto& exec = get_executor<CUDARuntime>();
 
-  DeviceBuffer<int>& dev_a = exec.allocate(1);
+  auto& dev_a = exec.allocate<int>(count);
 
-  Kernel vaddKernel =
+  auto vaddKernel =
       kernel<CUDARuntime>(test, {{(OPT_N + THREAD_N - 1) / THREAD_N}, {THREAD_N}});
 
   vaddKernel(dev_a.get());
 
-
-  dev_a.download(&a.begin(), sizeof(int));
-
   exec.synchronize();
 
+  dev_a.download(a.data(), count);
 
   std::cout << a[0] << std::endl;
   return 0;
