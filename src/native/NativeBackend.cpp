f07489b21725bb3fc063a7ad4e684e719c97d187
Lars Klein <l_klei14@wwu.de>
2016-10-20 16:21:18 +0200

    modified foo

--- src/native/NativeBackend.cpp
+++ src/native/NativeBackend.cpp
@@ -36,7 +36,7 @@ namespace {
   %5 = load i32, i32* %__z, align 4
   %6 = load i32, i32* %3, align 4
   %7 = icmp ult i32 %5, %6
-  br i1 %7, label %8, label %38
+  br i1 %7, label %8, label %35
 
   ; <label>:8                                       ; preds = %4
   store i32 0, i32* %__y, align 4
@@ -46,7 +46,7 @@ namespace {
   %10 = load i32, i32* %__y, align 4
   %11 = load i32, i32* %2, align 4
   %12 = icmp ult i32 %10, %11
-  br i1 %12, label %13, label %35
+  br i1 %12, label %13, label %32
 
   ; <label>:13                                      ; preds = %9
   store i32 0, i32* %__x, align 4
@@ -56,43 +56,43 @@ namespace {
   %15 = load i32, i32* %__x, align 4
   %16 = load i32, i32* %1, align 4
   %17 = icmp ult i32 %15, %16
-  br i1 %17, label %18, label %30
+  br i1 %17, label %18, label %27
 
   ; <label>:18                                      ; preds = %14
-  %19 = load i32, i32* %__x, align 4
-  %20 = zext i32 %19 to i64
-  %21 = load i32, i32* %__y, align 4
-  %22 = zext i32 %21 to i64
-  %23 = load i32, i32* %__z, align 4
-  %24 = zext i32 %23 to i64
+  %__xload = load i32, i32* %__x, align 4
+  %19 = zext i32 %__xload to i64
+  %__yload = load i32, i32* %__y, align 4
+  %20 = zext i32 %__yload to i64
+  %__zload = load i32, i32* %__z, align 4
+  %21 = zext i32 %__zload to i64
   call void @__dummy_kernel(i64 %20, i64 %22, i64 %24)
-  %25 = load i32, i32* %__x, align 4
-  %26 = add i32 %25, 1
-  store i32 %26, i32* %__x, align 4
-  %27 = load i32, i32* %__x, align 4
-  %28 = load i32, i32* %1, align 4
-  %29 = icmp ult i32 %27, %28
-  br i1 %29, label %18, label %30, !llvm.loop !4
+  %22 = load i32, i32* %__x, align 4
+  %23 = add i32 %25, 1
+  store i32 %23, i32* %__x, align 4
+  %24 = load i32, i32* %__x, align 4
+  %25 = load i32, i32* %1, align 4
+  %26 = icmp ult i32 %24, %25
+  br i1 %26, label %18, label %27, !llvm.loop !4
 
-  ; <label>:30                                      ; preds = %18
-  br label %31
+  ; <label>:27                                      ; preds = %18
+  br label %28
 
-  ; <label>:31                                      ; preds = %30
-  %32 = load i32, i32* %__y, align 4
-  %33 = add i32 %32, 1
-  store i32 %33, i32* %__y, align 4
+  ; <label>:28                                      ; preds = %30
+  %29 = load i32, i32* %__y, align 4
+  %30 = add i32 %29, 1
+  store i32 %30, i32* %__y, align 4
   br label %9
 
-  ; <label>:34                                      ; preds = %9
-  br label %35
+  ; <label>:31                                      ; preds = %9
+  br label %32
 
-  ; <label>:35                                      ; preds = %34
-  %36 = load i32, i32* %__z, align 4
-  %37 = add i32 %36, 1
-  store i32 %37, i32* %__z, align 4
+  ; <label>:32                                      ; preds = %34
+  %33 = load i32, i32* %__z, align 4
+  %34 = add i32 %33, 1
+  store i32 %34, i32* %__z, align 4
   br label %4
 
-  ; <label>:38                                      ; preds = %4
+  ; <label>:35                                      ; preds = %4
   ret void
 }
 
