0b1b27786d310be65cc8c9d3d628f0dd15df334e
Lars Klein <l_klei14@wwu.de>
2016-11-02 12:50:49 +0100

    test

--- src/native/NativeBackend.cpp
+++ src/native/NativeBackend.cpp
@@ -198,6 +198,7 @@ namespace pacxx
             TargetLibraryInfoImpl TLII(Triple(M.getTargetTriple()));
             _PM.add(new TargetLibraryInfoWrapperPass(TLII));
             _PM.add(createTargetTransformInfoWrapperPass(_machine->getTargetIRAnalysis()));
+            _PM.add(createScopedNoAliasAAWrapperPass());
             _PM.add(createPACXXAddrSpaceTransform());
             _PM.add(createPACXXNativeVectorizer());
             //_PM.add(createPACXXNativeLinker());
