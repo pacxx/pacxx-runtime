fcd4edfd7bc92c6430dd6ad9a4efdb1f17f0f090
Lars Klein <l_klei14@wwu.de>
2016-11-02 09:53:19 +0100

    added alias analysis

--- src/native/NativeBackend.cpp
+++ src/native/NativeBackend.cpp
@@ -196,11 +196,11 @@ namespace pacxx
             TargetLibraryInfoImpl TLII(Triple(M.getTargetTriple()));
             _PM.add(new TargetLibraryInfoWrapperPass(TLII));
             _PM.add(createTargetTransformInfoWrapperPass(_machine->getTargetIRAnalysis()));
+            _PM.add(createScopedNoAliasAAWrapperPass());
             _PM.add(createPACXXAddrSpaceTransform());
             _PM.add(createPACXXNativeKernelTransform());
-            _PM.add(createSCEVAAWrapperPass());
             _PM.add(createSLPVectorizerPass());
-            _PM.add(createPACXXNativeLinker());
+            //_PM.add(createPACXXNativeLinker());
             _pmInitialized = true;
         }
 
