b5ee9aa4445ecc65f1c12897363e862f2e166796
Lars Klein <l_klei14@wwu.de>
2016-11-10 11:47:25 +0100

    enabled O3

--- src/native/NativeBackend.cpp
+++ src/native/NativeBackend.cpp
@@ -227,10 +227,7 @@ namespace pacxx
             // add O3 optimizations, except vectorization
             PassManagerBuilder builder;
             builder.OptLevel = 3;
-            /*
             builder.populateModulePassManager(_PM);
-            builder.populateModulePassManager(_PM);
-            */
             _pmInitialized = true;
         }
 
