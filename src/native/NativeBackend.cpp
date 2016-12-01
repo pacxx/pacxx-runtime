1a2cc8a90b546ffb2ae83b7aa8890a50ad56af78
Lars Klein <l_klei14@wwu.de>
2016-12-01 10:37:21 +0100

    debug

--- src/native/NativeBackend.cpp
+++ src/native/NativeBackend.cpp
@@ -222,7 +222,7 @@ namespace pacxx
             _PM.add(createTargetTransformInfoWrapperPass(_machine->getTargetIRAnalysis()));
             _PM.add(createPACXXAddrSpaceTransform());
             _PM.add(createPACXXIdRemover());
-            _PM.add(createSPMDVectorizer(&M, _machine));
+            //_PM.add(createSPMDVectorizer(&M, _machine));
             //_PM.add(createPACXXNativeLinker());
             /*
             _PM.add(createPACXXNativeVectorizer(_machine));
