64e494848c2aeb298f542b0fa47d2c0f7c3c52e4
Lars Klein <l_klei14@wwu.de>
2016-12-05 17:25:45 +0100

    matmul and vecadd work

--- src/native/NativeBackend.cpp
+++ src/native/NativeBackend.cpp
@@ -229,7 +229,7 @@ namespace pacxx
             // add O3 optimizations
             PassManagerBuilder builder;
             builder.OptLevel = 3;
-            //builder.populateModulePassManager(_PM);
+            builder.populateModulePassManager(_PM);
             _pmInitialized = true;
         }
 
