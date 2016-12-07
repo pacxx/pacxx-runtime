084a39006a604a31678b6115a03269ebc1a4ffcf
Lars Klein <l_klei14@wwu.de>
2016-12-07 16:00:44 +0100

    refactor

--- src/native/NativeRuntime.cpp
+++ src/native/NativeRuntime.cpp
@@ -62,9 +62,6 @@ namespace pacxx
 
         legacy::PassManager PM = getPassManager();
         PM.add(createPACXXNativeRuntimeOpts(Kernel.getName(), config.threads.x, args));
-        PM.add(createSCCPPass());
-        PM.add(createDeadCodeEliminationPass());
-
         _CPUMod = _compiler->compile(*_M);
 
         void *fptr= nullptr;
