e3bdb56d96b7c5a3f49d5fbb09fc1551fc3db127
Lars Klein <l_klei14@wwu.de>
2017-01-13 16:13:30 +0100

    kernel_with_CB

--- include/PACXX.h
+++ include/PACXX.h
@@ -222,7 +222,7 @@ auto kernel(const Func &lambda, KernelConfiguration config) {
   return _kernel<decltype(lambda), RuntimeT, versioning>(lambda, config);
 };
 
-template <typename Func, typename CallbackFunc, typename RuntimeT, size_t versioning = __COUNTER__>
+template <typename Func, typename CallbackFunc, typename RuntimeT = CUDARuntime, size_t versioning = __COUNTER__>
 auto kernel_with_cb(const Func &lambda, KernelConfiguration config,
                     CallbackFunc &&CB) {
   return _kernel_with_cb<decltype(lambda), CallbackFunc, RuntimeT, versioning>(
