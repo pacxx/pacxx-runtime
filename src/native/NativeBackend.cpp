1b3ec7dc0d2aae1b6372545c5f00c498433e3cc0
Lars Klein <l_klei14@wwu.de>
2017-03-23 09:54:30 +0100

    disabled asm printing because of segfault

--- src/native/NativeBackend.cpp
+++ src/native/NativeBackend.cpp
@@ -234,9 +234,12 @@ void NativeBackend::applyPasses(Module& M) {
             _pmInitialized = true;
         }
 
+        /*
         std::error_code EC;
         raw_fd_ostream OS2("assembly.asm", EC, sys::fs::F_None);
         _machine->addPassesToEmitFile(_PM, OS2, TargetMachine::CGFT_AssemblyFile);
+         */
+
 
         _PM.run(M);
 }
