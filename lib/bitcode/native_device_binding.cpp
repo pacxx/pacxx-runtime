3bbe95d28e29c5f6618257e460e21ffd226a0a4d
Michael Haidl <michael.haidl@uni-muenster.de>
2017-08-11 13:35:06 +0200

    fixed compilation errors when PACXX gets confused with libc++ headers

--- lib/bitcode/native_device_binding.cpp
+++ lib/bitcode/native_device_binding.cpp
@@ -1,6 +1,7 @@
 #include "pacxx/detail/device/DeviceCode.h"
 #include "pacxx/detail/device/DeviceFunctionDecls.h"
-#include <math.h>
+#include </usr/include/math.h> // FIXME: PACXX gets confused with cmath from libc++ on some platforms thats
+                               //        why we include math.h with an absolute path here
 
 __forceinline__ __index_t get_global_id(unsigned int dimindx) {
   switch (dimindx) {
