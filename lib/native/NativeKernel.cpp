cb06d8269424ca02c83324a5f2887436d367053c
Michael Haidl <michael.haidl@uni-muenster.de>
2017-11-14 14:29:52 +0100

    updated project name in sourcefiles

--- lib/native/NativeKernel.cpp
+++ lib/native/NativeKernel.cpp
@@ -1,6 +1,6 @@
 //===-----------------------------------------------------------*- C++ -*-===//
 //
-//                     The LLVM Compiler Infrastructure
+//                       The LLVM-based PACXX Project
 //
 // This file is distributed under the University of Illinois Open Source
 // License. See LICENSE.TXT for details.
