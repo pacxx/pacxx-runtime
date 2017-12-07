833153eb4adac500df4b43efdffe3c6e49e5a886
Michael Haidl <michael.haidl@uni-muenster.de>
2017-12-07 09:58:04 +0100

    removed a lot of llvm headers from pacxx headers to avoid pulling comand line options into client code using Executor

--- lib/Executor.cpp
+++ lib/Executor.cpp
@@ -8,23 +8,32 @@
 //===----------------------------------------------------------------------===//
 
 #include "pacxx/Executor.h"
+#include "pacxx/detail/common/ExecutorHelper.h"
 
 namespace pacxx {
 namespace v2 {
 
-Executor::Executor(std::unique_ptr<IRRuntime> &&rt) :
-    _ctx(new LLVMContext()), _runtime(std::move(rt)){
+std::vector<Executor> *getExecutorMemory() {
+
+  static std::vector<Executor> *executors = new std::vector<Executor>();
+  return executors; // TODO: free resources at application's exit
+}
+
+std::vector<Executor> &Executor::getExecutors() { *getExecutorMemory(); }
+
+Executor::Executor(std::unique_ptr<IRRuntime> &&rt)
+    : _ctx(new LLVMContext()), _runtime(std::move(rt)) {
   core::CoreInitializer::initialize();
 }
 
-Executor::Executor(Executor &&other)  {
+Executor::Executor(Executor &&other) {
   _ctx = std::move(other._ctx);
   _runtime = std::move(other._runtime);
   _id = other._id;
   _kernel_translation = std::move(other._kernel_translation);
 }
 
-void Executor::setModule(std::unique_ptr <llvm::Module> M) {
+void Executor::setModule(std::unique_ptr<llvm::Module> M) {
   _runtime->link(std::move(M));
 
   auto &nM = _runtime->getModule();
@@ -32,26 +41,25 @@ void Executor::setModule(std::unique_ptr <llvm::Module> M) {
     _kernel_translation[cleanName(F.getName().str())] = F.getName().str();
 }
 
-Executor &get_executor(unsigned id) {
-  return Executor::get(id);
-}
+Executor &get_executor(unsigned id) { return Executor::get(id); }
 
 unsigned Executor::getID() { return _id; }
 
 ExecutingDevice Executor::getExecutingDeviceType() {
   switch (_runtime->getKind()) {
 #ifdef PACXX_ENABLE_CUDA
-  case IRRuntime::RuntimeKind::RK_CUDA: return ExecutingDevice::GPUNvidia;
+  case IRRuntime::RuntimeKind::RK_CUDA:
+    return ExecutingDevice::GPUNvidia;
 #endif
-  case IRRuntime::RuntimeKind::RK_Native: return ExecutingDevice::CPU;
-  case IRRuntime::RuntimeKind::RK_HIP: return ExecutingDevice::GPUAMD;
+  case IRRuntime::RuntimeKind::RK_Native:
+    return ExecutingDevice::CPU;
+  case IRRuntime::RuntimeKind::RK_HIP:
+    return ExecutingDevice::GPUAMD;
   }
   llvm_unreachable("unknown runtime kind");
 }
 
-size_t Executor::getConcurrentCores() {
-  return _runtime->getConcurrentCores();
-}
+size_t Executor::getConcurrentCores() { return _runtime->getConcurrentCores(); }
 
 std::string Executor::cleanName(const std::string &name) {
   auto cleaned_name =
@@ -77,7 +85,9 @@ RawDeviceBuffer &Executor::allocateRaw(size_t bytes, MemAllocMode mode) {
   return *_runtime->allocateRawMemory(bytes, mode);
 }
 
-void Executor::freeRaw(RawDeviceBuffer &buffer) { _runtime->deleteRawMemory(&buffer); }
+void Executor::freeRaw(RawDeviceBuffer &buffer) {
+  _runtime->deleteRawMemory(&buffer);
+}
 
 IRRuntime &Executor::rt() { return *_runtime; }
 
@@ -101,7 +111,6 @@ void registerModule(const char *start, const char *end) {
   __moduleStart(start);
   __moduleEnd(end);
 }
-
-}
-}
+} // namespace v2
+} // namespace pacxx
 
