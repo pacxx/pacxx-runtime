936366c77bb3b438e8372f801086f2b5b8334277
Michael Haidl <michael.haidl@uni-muenster.de>
2017-12-07 12:23:21 +0100

    fixed rsqrt in native device binding

--- lib/bitcode/native_device_binding.cpp
+++ lib/bitcode/native_device_binding.cpp
@@ -10,14 +10,17 @@
 #include </usr/include/math.h> // FIXME: PACXX gets confused with cmath from libc++ on some platforms thats
                                //        why we include math.h with an absolute path here
 
+#include "pacxx/detail/device/DeviceCode.h"
+#include "pacxx/detail/device/DeviceFunctionDecls.h"
+
 #ifndef __forceinline__
 #define __forceinline__ __attribute__((always_inline))
 #endif
 
-extern "C" __forceinline__ double rsqrt(double val) {
+extern "C" double rsqrt(double val) {
   return 1.0 / sqrt(val);
 }
 
-extern "C" __forceinline__ float rsqrtf(float val) {
+extern "C" float rsqrtf(float val) {
   return 1.0f / sqrtf(val);
 }
